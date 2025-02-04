b5c2866bc8900e3a99e3d7774c247ca0a6ca6d80
vamsi krishna <iamvkrishna24@gmail.com>
2025-02-04 20:21:46 +0530

    lataest update

---
+++ Backend/db_handler.py
@@ -0,0 +1,48 @@
+import sqlite3
+
+def execute_query(query,params=(),fetch = False):   #function FIRST
+    connect = sqlite3.connect('Dictionary.db')
+    cursor = connect.cursor()
+
+    cursor.execute(query,params)
+
+    if fetch:
+        result = cursor.fetchall()
+        connection.close()
+        return result
+    
+    connect.commit()
+    connect.close()
+
+
+def get_word_meaning(word):                         #Function SECOND
+    query='''
+    SELECT definition, examples
+    FROM dictioanry
+    WHERE word = ?
+    '''
+
+    result = execute_query(query, (word,),fetch = True)
+
+    if result:
+        definition, examples = result[0]
+        return{
+            'word':word,
+            'definition':definition,
+            'examples':examples.split(',')
+        }
+    else:
+        return None
+
+
+def insert_word(word1,synonm,definition,examples):          #THIRD Function 
+    query = '''
+    INSERT INTO dictionary(word1,synonm,definition,examples)
+    VALUES (?,?,?,?)
+    '''
+
+    synonm_str = ','.join(synonm) if synonm else ''
+    example_str = ','.join(examples) if examples else ''
+
+    execute_query(query, (word1,synonm_str,efinition,example_str))
+
